d2fce3b374277392291304914bfe012d07ad63e3
Tim L <144381708+UnlistedTim@users.noreply.github.com>
2026-03-19 23:33:45 -0400

    Update TeleopState.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopState.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopState.java
@@ -316,6 +316,13 @@ public class TeleopState extends LinearOpMode  {
 
             turntable(gamepad2.right_stick_x);
 
+            telemetry.addData("Left intake current", intakeLeft.getCurrent(CurrentUnit.MILLIAMPS));
+            telemetry.addData("Right intake current", intakeRight.getCurrent(CurrentUnit.MILLIAMPS));
+
+            telemetry.update();
+
+
+
 
 
 
